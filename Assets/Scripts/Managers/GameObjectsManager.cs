eb82b7424c700a748e13f3f14e6c2b43bcc257c9
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-02-06 20:02:38 +0300

    added instantiating of game objects

--- Assets/Scripts/Managers/GameObjectsManager.cs
+++ Assets/Scripts/Managers/GameObjectsManager.cs
@@ -1,12 +1,35 @@
-﻿using UnityEngine;
+﻿using GameObjects.Ball;
+using GameObjects.Field;
+using GameObjects.Racket;
+using UnityEngine;
 
 namespace Managers
 {
     public class GameObjectsManager : MonoBehaviour
     {
+        public Transform GameObjectsContainerPrefab;
+        public FieldView FieldViewPrefab;
+        public RacketView RacketViewPrefab;
+        public BallView BallViewPrefab;
+
+        private Transform _gameObjectsContainerInstance;
+        private FieldView _fieldViewInstance;
+        private RacketView _topRacketViewInstance;
+        private RacketView _bottomRacketViewInstance;
+        private BallView _ballViewInstance;
+
         public void Initialize()
         {
+            _gameObjectsContainerInstance = Instantiate(GameObjectsContainerPrefab);
 
+            _fieldViewInstance = Instantiate(FieldViewPrefab, _gameObjectsContainerInstance.gameObject.transform);
+
+            _topRacketViewInstance = Instantiate(RacketViewPrefab, _fieldViewInstance.gameObject.transform);
+            _topRacketViewInstance.gameObject.transform.position = new Vector3(0, 4.5f, 0);
+            _bottomRacketViewInstance = Instantiate(RacketViewPrefab, _fieldViewInstance.gameObject.transform);
+            _bottomRacketViewInstance.gameObject.transform.position = new Vector3(0, -4.5f, 0);
+
+            _ballViewInstance = Instantiate(BallViewPrefab, _fieldViewInstance.gameObject.transform);
         }
     }
 }
